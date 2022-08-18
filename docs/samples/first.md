0eb82de3c0a278714c2eb2b17a2650a5ba27c5e8
Eduardo Schettino <schettino72@gmail.com>
2022-08-18 08:41:21 +0700

    move ZGraph to component file.

--- docs/samples/first.md
+++ docs/samples/first.md
@@ -2,51 +2,13 @@
 sidebar_position: 6
 ---
 
-import * as zrender from '@site/static/zrender.js';
-import CodeBlock from '@theme/CodeBlock';
+import ZGraph from '@site/src/components/ZGraph.js';
 
-export class ZGraph extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.$container = React.createRef();
-    }
-    componentDidMount() {
-        var zr = zrender.init(this.$container.current);
-        this.props.renderGraph(zr);
-    }
-    render() {
-        return (
-          <div>
-            <CodeBlock language="js">
-              {this.props.src}
-            </CodeBlock>
-            <div className="content">
-              <div ref={this.$container} className="example-container">
-              </div>
-            </div>
-          </div>
-        );
-    }
-}
-
-export function trimLines(text, nStart, nEnd) {
-    var lines = text.split('\n');
-    // remove lines, starting at the beginning of file
-    lines.splice(0, nStart);
-    if (nEnd) {
-      lines.splice(lines.length - nEnd, nEnd);
-    }
-    return lines.join('\n');
-}
-
-<!-- TODO: replace raw-loader on webpack 5 -->
-<!-- https://webpack.js.org/guides/asset-modules/#replacing-inline-loader-syntax -->
 import simpleCircle from '@site/snippets/circle.js';
 import srcCircle from '!!raw-loader!@site/snippets/circle.js';
 
 
 <ZGraph 
-    src={trimLines(srcCircle, 3, 3)}
+    src={srcCircle} srcTrim={[3, 3]}
     renderGraph={simpleCircle}>
 </ZGraph>
---
+++ src/components/ZGraph.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import * as zrender from '@site/static/zrender.js';
+
+import CodeBlock from '@theme/CodeBlock';
+
+
+export function trimLines(text, nStart, nEnd) {
+    var lines = text.split('\n');
+    // remove lines, starting at the beginning of file
+    lines.splice(0, nStart);
+    if (nEnd) {
+        lines.splice(lines.length - nEnd, nEnd);
+    }
+    return lines.join('\n');
+}
+export default class ZGraph extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.$container = React.createRef();
+        if (this.props.srcTrim) {
+            this.src = trimLines(this.props.src, this.props.srcTrim[0], this.props.srcTrim[1]);
+        } else {
+            this.src = this.props.src;
+        }
+        this.renderGraph = this.props.renderGraph;
+    }
+    componentDidMount() {
+        // TODO: replace raw-loader on webpack 5.
+        // https://webpack.js.org/guides/asset-modules/#replacing-inline-loader-syntax
+        // needs to wait for docusaurus ESM? https://github.com/facebook/docusaurus/issues/6520
+        // if (this.props.snippet) {
+        //   let draw = await import(this.props.snippet);
+        //   this.renderGraph = draw;
+        // }
+        var zr = zrender.init(this.$container.current);
+        this.renderGraph(zr);
+    }
+    render() {
+        return (
+            <div>
+                <CodeBlock language="js">
+                    {this.src}
+                </CodeBlock>
+                <div className="content">
+                    <div ref={this.$container} className="example-container">
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
